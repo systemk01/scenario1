7f904bcfe8160b31d4c83fdd7090042ecf99d5bb
systemk01 <systemk1@icloud.com>
2025-03-15 00:54:26 +0100

    first commit, TCP-Server without errorhandling

---
+++ tcpserver/src/main.rs
@@ -0,0 +1,18 @@
+use std::net::TcpListener;
+use std::io::{Read, Write};
+
+
+fn main() {
+    
+    let connection_listener = TcpListener::bind("127.0.0.1:3080").unwrap();
+    println!("Server listening on port 3080");
+
+    for stream in connection_listener.incoming() {
+        let mut stream = stream.unwrap();
+        println!("Connection established!");
+
+        let mut buffer = [0; 1024];
+        stream.read(&mut buffer).unwrap();
+        stream.write(&mut buffer).unwrap();
+    }
+}
